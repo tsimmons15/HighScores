1a194d0d09cf32662f7700afdab38e3b9cf4161c
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-04-21 11:51:23 -0400

    Implemented sorter

--- src/main/java/dev/simmons/sorting/Sorter.java
+++ src/main/java/dev/simmons/sorting/Sorter.java
@@ -3,15 +3,17 @@ package dev.simmons.sorting;
 import dev.simmons.models.Score;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Sorter implements Sorting{
     @Override
     public List<Score> sortByInitials(List<Score> scores, String initials) {
-        return null;
+        return scores.stream().filter(s -> s.getInitials().equals(initials)).collect(Collectors.toList());
     }
 
     @Override
     public List<Score> sortByScore(List<Score> scores) {
-        return null;
+        scores.sort((s, o) -> Integer.compare(s.getPoints(), o.getPoints()))
+        return scores;
     }
 }
