7a2a01fa05f0f646c0fb42d1c32bfc9425728470
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-04-21 11:01:20 -0400

    init

---
+++ src/test/java/dev/simmons/models/ExampleTest.java
@@ -0,0 +1,11 @@
+package dev.simmons.models;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    public void tautology() {
+        Assertions.assertTrue(1 == 1);
+    }
+}
